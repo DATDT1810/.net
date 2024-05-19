1643cc8946b81505733bea4b0b550542807db2d7
TanDat <147079917+DATDT1810@users.noreply.github.com>
2024-05-20 00:32:05 +0700

    Create project

---
+++ MyProjectClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyProjectClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
